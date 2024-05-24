722e847a12ea13fb9a2ed6879a65f7ba63057f70
HanifAjiPrasetyo <hanifaji007@gmail.com>
2024-05-24 13:06:27 +0700

    fix details

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-import Login from  './pages/Login.jsx'
-import RegistrationForm from './pages/Register.jsx'
+import Login from "./pages/Login.jsx";
+import RegistrationForm from "./pages/Register.jsx";
 import ProductPage from "./pages/product";
 import DetailProduct from "./pages/detailProduct";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Helmet } from "react-helmet";
 
 function App() {
   return (
     <div className="min-h-screen font-rubik">
       <Router>
         <Routes>
-          <Route path="/" element={<Login/>} />
-          <Route path="register" element={<RegistrationForm/>} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<RegistrationForm />} />
           <Route path="/product" element={<ProductPage />} />
           <Route path="/product/:slug" element={<DetailProduct />} />
         </Routes>
