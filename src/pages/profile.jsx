8ac48899eb8da35814f0cd3ed2307b7710b49a88
Az Zikra Ismunandar <azzikra.ismunandar08@gmail.com>
2024-05-29 16:16:41 +0700

    add `profile.jsx`

---
+++ src/pages/profile.jsx
@@ -0,0 +1,42 @@
+import Header from "../components/Header";
+import CardSettings from "../components/CardSettings";
+import Footer from "../components/Footer";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setUser } from "../slices/authSlice";
+import { getUser } from "../api";
+
+const Profile = () => {
+  useEffect(() => {
+    document.title = "JO'E Cape | Profile";
+  }, []);
+
+  // Fetch User Data
+  const dispatch = useDispatch();
+  const { token } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const data = await getUser(token);
+        dispatch(setUser(data));
+      } catch (error) {
+        return;
+      }
+    };
+    fetchUser();
+  }, [token]);
+
+  return (
+    <div className="flex flex-col">
+      <Header title={"Profile"} />
+      <div className="flex justify-center flex-wrap mt-10">
+        <div className="w-full px-4">
+          <CardSettings />
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+export default Profile;
